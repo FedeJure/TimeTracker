0fe559565f18bbf4587b4ebe446bb0d9e1dd5133
federico <fjure@atixlabs.com>
2019-05-22 15:37:22 -0300

    temportal

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-
 sudo apt-get update
 sudo apt-get install python-pip -y
 sudo apt-get install python-tk -y
 pip install -r requirements.txt
+
+sudo ln timetracker.sh /usr/local/bin/timetracker
 #etc.
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import logging
 import time
 from daemonize import Daemonize
+import glob
 import os
 
 pid = "/tmp/timeTracking.pid"
@@ -14,33 +15,52 @@ fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
 actualPath = os.path.dirname(os.path.abspath(__file__))
+filePathPrefix = actualPath + "/reports"
 
 _lapse = 1800
 
 
 
-def main():
-    savedTime = time.localtime(time.time())
-    logger.debug("Time Tracking started...")
-    filepath = "{}/{}-{}-{}_{}:{}.txt".format(actualPath,savedTime.tm_mday,savedTime.tm_mon,
+def main(latest_file=None):
+  savedTime = time.localtime(time.time())
+  if (not latest_file):
+    logger.debug("Creating new report file")
+    filepath = "{}/{}-{}-{}_{}:{}.txt".format(filePathPrefix,savedTime.tm_mday,savedTime.tm_mon,
                                         savedTime.tm_year,savedTime.tm_hour,savedTime.tm_min)
-    logger.debug("saving file on {}".format(actualPath + "/" + filepath))
+    logger.debug("saving file on {}".format(filepath))
     with open(filepath, "w") as f:
+      logger.debug(f)
       f.write("Time tracking for date {}".format(filepath))
-
-    while True:
-      code = os.system("python {}/timeTracker.py {}".format(actualPath,filepath))
-      logger.debug("Exit code: {}".format(code))
-      if (code == 512):
-        daemon.close()
-        break
-      time.sleep(_lapse)
-
-    
-    
+  else:
+    filepath = latest_file
 
 
+  while True:
+    code = os.system("python {}/timeTracker.py {}".format(actualPath,filepath))
+    logger.debug("Exit code: {}".format(code))
+    if (code == 512):
+      daemon.close()
+      break
+    time.sleep(_lapse)
 
-daemon = Daemonize(app="time_tracking", pid=pid, action=main, keep_fds=keep_fds)
-daemon.start()
+
+
+
+def start():
+  is_running = os.path.isfile(pid) 
+  if (is_running):
+    logger.debug("Proccess already started, recovering...")
+    list_of_files = glob.glob('{}/*.txt'.format(filePathPrefix))
+    if (len(list_of_files) > 0):        
+      latest_file = max(list_of_files, key=os.path.getctime)
+      main(latest_file)
+    exit(1)
+  else:
+    logger.debug("Time Tracking started...")
+    daemon = Daemonize(app="time_tracking", pid=pid, action=main, keep_fds=keep_fds)
+    daemon.start()
+
+start()
+
+
 
